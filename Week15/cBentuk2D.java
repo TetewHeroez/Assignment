f4d2a95e5968b5ee96b3f5b4ab2ceaa79ef56ccc
TetewHeroez <teosofihidayahagung@gmail.com>
2024-06-13 12:09:55 +0700

    liang

---
+++ Week15/cBentuk2D.java
@@ -0,0 +1,22 @@
+package Aplro2.Week15;
+
+public abstract class cBentuk2D {
+    private String warna;
+
+    public cBentuk2D() {
+        warna = "Transparan";
+    }
+    public cBentuk2D(String warna) {
+        this.warna = warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+    public String getWarna() {
+        return warna;
+    }
+    
+    public abstract double hitungLuas();
+    public abstract double hitungKeliling();
+}
